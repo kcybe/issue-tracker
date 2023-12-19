f3acb52e416e8f56e97a8217c15c91dc06ccc91e
kcybe <kcybe@github.com>
2023-12-20 00:53:20 +0200

    Issue Tracker project, using React, MUI5 Joy, Typescript, TailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Home from './home/Home'
+import Issues from './issues/Issues'
+import { BrowserRouter, Route, Routes} from "react-router-dom";
+import { CssVarsProvider } from "@mui/joy";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <CssVarsProvider defaultMode="system">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/issues' element={<Issues />}/>
+        </Routes>
+      </CssVarsProvider>
+    </BrowserRouter>
   );
 }
 
