6870d19c2e3eba3a6d5a94f4e59f643996dedf07
unknown <kcybe0x1@github.com>
2023-12-20 13:36:07 +0200

    Added Issue Modal

---
+++ src/issues/components/IssueModal.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import Button from '@mui/joy/Button';
+import FormControl from '@mui/joy/FormControl';
+import FormLabel from '@mui/joy/FormLabel';
+import Input from '@mui/joy/Input';
+import Modal from '@mui/joy/Modal';
+import ModalDialog from '@mui/joy/ModalDialog';
+import DialogTitle from '@mui/joy/DialogTitle';
+import DialogContent from '@mui/joy/DialogContent';
+import Stack from '@mui/joy/Stack';
+import Add from '@mui/icons-material/Add';
+import Select from '@mui/joy/Select';
+import Option from '@mui/joy/Option';
+
+
+export default function IssueModal () {
+  const [open, setOpen] = React.useState<boolean>(false);
+    return (
+      <React.Fragment>
+        <Button
+          variant="outlined"
+          color="neutral"
+          startDecorator={<Add />}
+          onClick={() => setOpen(true)}
+        >
+          New Issue
+        </Button>
+        <Modal open={open} onClose={() => setOpen(false)}>
+          <ModalDialog>
+            <DialogTitle>Open a new issue</DialogTitle>
+            <DialogContent>Fill in the information of the issue.</DialogContent>
+            <form
+              onSubmit={(event: React.FormEvent<HTMLFormElement>) => {
+                event.preventDefault();
+                setOpen(false);
+              }}
+            >
+              <Stack spacing={2}>
+                <FormControl>
+                  <FormLabel>Title</FormLabel>
+                  <Input autoFocus required />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Description</FormLabel>
+                  <Input required />
+                </FormControl>
+                <FormControl>
+                <FormLabel>Priority</FormLabel>
+                  <Select defaultValue="Medium">
+                    <Option value="low">Low</Option>
+                    <Option value="medium">Medium</Option>
+                    <Option value="high">High</Option>
+                  </Select>
+                </FormControl>
+                <Button type="submit">Submit</Button>
+              </Stack>
+            </form>
+          </ModalDialog>
+        </Modal>
+      </React.Fragment>
+    );  
+}
